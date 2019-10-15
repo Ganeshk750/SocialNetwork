0d688fec41690b8754024764cc7ddc0cc90822f1
Ganeshk750 <ganesh9ot@gmail.com>
2019-10-15 21:38:36 +0530

    adding models files and their routes

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const app = express();
 //DATABASE CONNECT
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+//INIT MIDDLEWARE
+app.use(express.json({ extended: false }));
 
 app.get('/',(req,res) =>{
     res.send('API is working');
 });
 
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is Running on ${PORT}`));
