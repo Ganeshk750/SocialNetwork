aa434c5191100edc80cd6881528bbde34ca2caba
Ganeshk750 <ganesh9ot@gmail.com>
2019-10-15 22:52:22 +0530

    adding jsonwebtoken module

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require('express-validator');//check we also add
 const User = require('../../models/User');
 
@@ -58,7 +60,18 @@ router.post(
     await user.save();
 
     //Return jsonwebtoken
-      res.send("Users Registerd");  
+    const payload = {
+        user:{
+            id: user.id,
+        }
+    }
+    jwt.sign(payload, config.get('jwtSecret'),
+    {expiresIn:36000},
+    (err, token)=>{
+        if(err) throw err;
+        res.json({token});
+    });
+    //res.send("Users Registerd");  
     }catch(err){
       console.error(err.message);
       res.status(500).send('Server error');
